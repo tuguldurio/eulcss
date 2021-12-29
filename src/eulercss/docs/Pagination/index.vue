0a98822e58b030767f6bfa7bc57c864708f0dc73
tuguldurio <on.tuguldur@gmail.com>
2021-12-29 16:27:23 -0500

    Arrow pagination

--- src/eulercss/docs/Pagination/index.vue
+++ src/eulercss/docs/Pagination/index.vue
@@ -3,6 +3,7 @@ import Simple from './Simple.html?raw'
 import Circle from './Circle.html?raw'
 import WithBorder from './WithBorder.html?raw'
 import FullWidth from './FullWidth.html?raw'
+import Arrows from './Arrows.html?raw'
 </script>
 
 <template>
@@ -11,5 +12,6 @@ import FullWidth from './FullWidth.html?raw'
     <SimplePreview title="Circle" :code="Circle"/>
     <SimplePreview title="With Border" :code="WithBorder"/>
     <SimplePreview title="Full Width with Text" :code="FullWidth"/>
+    <SimplePreview title="Arrows" :code="Arrows"/>
   </DocsContainer>
 </template>
