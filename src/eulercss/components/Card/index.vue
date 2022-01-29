21f46504e2096b686bdb7c5cc43a6cb03a54676a
tuguldurio <on.tuguldur@gmail.com>
2022-01-29 14:27:18 -0500

    card components

--- src/eulercss/components/Card/index.vue
+++ src/eulercss/components/Card/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import Simple from './Simple.html?raw'
+import WithImage from './WithImage.html?raw'
+import Product from './Product.html?raw'
 </script>
 
 <template>
   <DocsContainer>
     <IFramePreview title="Simple" :code="Simple" :paddingX="false"/>
+    <IFramePreview title="With Image" :code="WithImage" :paddingX="false"/>
+    <IFramePreview title="Product" :code="Product" :paddingX="false"/>
   </DocsContainer>
 </template>
